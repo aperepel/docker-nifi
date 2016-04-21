d3054bc40fde1c904142e9c6fca41ca61aa2c358
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-04-21 10:04:59 -0400

    fix seds, allow env vars to set min/max heap

--- docker/run.sh
+++ docker/run.sh
@@ -13,14 +13,18 @@ configure_common() {
   sed -i 's/\.\/database_repository/\/databaserepo/g' $NIFI_HOME/conf/nifi.properties
   sed -i 's/\.\/provenance_repository/\/provenancerepo/g' $NIFI_HOME/conf/nifi.properties
 
-  sed -i 's/nifi\.kerberos\.krb5\.file=*$/nifi\.kerberos\.krb5\.file=\/etc\/krb5.conf/g' $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.ui\.banner\.text=*$/nifi.ui.banner.text=${NIFI_UI_BANNER_TEXT}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i 's/nifi\.kerberos\.krb5\.file=.*$/nifi\.kerberos\.krb5\.file=\/etc\/krb5.conf/g' $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.ui\.banner\.text=.*$/nifi.ui.banner.text=${NIFI_UI_BANNER_TEXT}/g" $NIFI_HOME/conf/nifi.properties
+
+  sed -i "s/#java\.arg\.13=-XX:+UseG1GC/java\.arg\.13=-XX:+UseG1GC/g" $NIFI_HOME/conf/bootstrap.conf
+  sed -i "s/java\.arg\.2=-Xms.*$/java\.arg\.2=$NIFI_JAVA_MINHEAP/g" $NIFI_HOME/conf/bootstrap.conf
+  sed -i "s/java\.arg\.3=-Xmx.*$/java\.arg\.3=$NIFI_JAVA_MAXHEAP/g" $NIFI_HOME/conf/bootstrap.conf
 }
 
 configure_site2site() {
   # configure the receiving end of site2site
-  sed -i "s/nifi\.remote\.input\.socket\.host=*$/nifi.remote.input.socket.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=*$/nifi.remote.input.socket.port=12345/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.host=.*$/nifi.remote.input.socket.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*$/nifi.remote.input.socket.port=12345/g" $NIFI_HOME/conf/nifi.properties
   # unsecure for now so we don't complicate the setup with certificates
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" $NIFI_HOME/conf/nifi.properties
 }
@@ -30,20 +34,20 @@ configure_cluster_node() {
   # does not resolve it back to the node. If it's set to the ${HOSTNAME}, the cluster works,
   # but the node's web ui is not accessible on the external network
   #sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=0.0.0.0/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.web\.http\.host=*$/nifi.web.http.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=.*$/nifi.web.http.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=*$/nifi.cluster.node.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=*$/nifi.cluster.node.protocol.port=12346/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.address=.*$/nifi.cluster.node.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*$/nifi.cluster.node.protocol.port=12346/g" $NIFI_HOME/conf/nifi.properties
   # the following properties point to the NCM - note we are using the network alias (implicitly created by docker-compose)
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=*$/nifi.cluster.node.unicast.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=*$/nifi.cluster.node.unicast.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=.*$/nifi.cluster.node.unicast.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=.*$/nifi.cluster.node.unicast.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
 }
 
 configure_cluster_manager() {
-  sed -i "s/nifi\.web\.http\.host=*$/nifi.web.http.host=0.0.0.0/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=.*$/nifi.web.http.host=0.0.0.0/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.manager=false/nifi.cluster.is.manager=true/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.manager\.address=*$/nifi.cluster.manager.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.manager\.protocol\.port=*$/nifi.cluster.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.manager\.address=.*$/nifi.cluster.manager.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.manager\.protocol\.port=.*$/nifi.cluster.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
 }
 
 splash
